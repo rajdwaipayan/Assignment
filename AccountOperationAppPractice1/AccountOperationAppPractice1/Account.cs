717cf669b6144d99208138342753c43c17fd0e62
gulfam7 <shaju9218@gmail.com>
2019-10-15 09:50:48 +0600

    Day1_Added

---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    public class Account
+    {
+        public string AccountNo;
+        public string Name;
+        public double Balance;
+        public double Amount;
+        public void Withdraw(Account account)
+        {
+            Balance -= account.Amount;
+
+        }
+
+        public void Deposit(Account account)
+        {
+            Balance += account.Amount;
+        }
+
+        public string Report()
+        {
+            string message = Name +",your account Number: "+AccountNo +"and  its Balance "+Balance+"Taka";
+            return message;
+        }
+    }
+
+}
---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        Account account=new Account();
+
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+
+
+
+   
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void CreateButton_Click_1(object sender, EventArgs e)
+        {
+            account.AccountNo = accountNoTextBox.Text;
+            account.Name = customerNameTextBox.Text;
+
+            accountNoTextBox.Clear();
+            customerNameTextBox.Clear();
+        }
+
+        private void DipositButton_Click_1(object sender, EventArgs e)
+        {
+            account.Amount = Convert.ToDouble(amountTextBox.Text);
+            account.Deposit(account);
+
+        }
+
+        private void WithdrawButton_Click_1(object sender, EventArgs e)
+        {
+            account.Amount = Convert.ToDouble(amountTextBox.Text);
+            account.Withdraw(account);
+        }
+
+        private void ReportButton_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(account.Report());
+        }
+    }
+}
